68806cd80cd0751c8689699271e6b8f74f313bf9
Sunset3777 <aua52257@gmail.com>
2026-01-06 15:03:51 +0800

    feat: add new event funton

---
+++ script.js
@@ -0,0 +1,33 @@
+//新增新的待辦事項
+const inputText = document.querySelector('#inputtext input')
+const addButton = document.querySelector('#inputtext a')
+
+const ul = document.querySelector('#list')
+
+function addItem(e){
+    e.preventDefault();
+
+    if (inputText.value ==='') {
+        alert('不能輸入空的待辦事項');
+        return;
+    }
+
+    const li = document.createElement('li')
+    
+    li .innerHTML =
+    `<label class="todoList_label">
+    <input class="todoList_input" type="checkbox" value="true">
+    <span>${inputText.value}</span>
+    </label>
+    <a href="#">
+    <i class="fa fa-times"></i>
+    </a>`
+    
+    ul .appendChild(li);
+
+    inputText.value ='';
+
+    console.log('成功加入新待辦事項!');
+}
+
+addButton.addEventListener('click',addItem)
