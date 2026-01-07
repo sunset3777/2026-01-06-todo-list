a3433eed0fff2a371ea99dad3f79e3e1ed58cc79
Sunset3777 <aua52257@gmail.com>
2026-01-07 10:09:15 +0800

    feat: add read function

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+//renderData儲存陣列
+let tododata=[{
+    content:'做todo list !',
+    checked:false
+}];
+
 //新增新的待辦事項
 const inputText = document.querySelector('#inputtext input')
 const addButton = document.querySelector('#inputtext a')
@@ -11,21 +17,14 @@ function addItem(e){
         return;
     }
 
-    const li = document.createElement('li')
-    
-    li .innerHTML =
-    `<label class="todoList_label">
-    <input class="todoList_input" type="checkbox" value="true">
-    <span>${inputText.value}</span>
-    </label>
-    <a href="#">
-    <i class="fa fa-times"></i>
-    </a>`
-    
-    ul .appendChild(li);
+    const newtodo ={
+        content:inputText.value,
+        checked:false
+    }
 
-    inputText.value ='';
-
+    tododata.push(newtodo);
+    inputText.value = '';
+    renderData();
     console.log('成功加入新待辦事項!');
 }
 
@@ -36,7 +35,7 @@ ul.addEventListener('click', deleteItem);
 
 
 function deleteItem(e) {
-    
+
     if (e.target.classList.contains('fa-times')) {
         e.preventDefault();
         const targetLi = e.target.closest('li');
@@ -44,4 +43,26 @@ function deleteItem(e) {
         
         console.log('項目已刪除');
     }
+}
+
+//新增renderData來控制輸入的待辦事項
+function renderData(){
+    console.log("渲染函式啟動了！");
+    let str="";
+    tododata.forEach(function(item, index){
+        str +=`<li>
+                <label class="todoList_label">
+                    <input class="todoList_input" type="checkbox" 
+                           ${item.checked ? 'checked' : ''} 
+                           data-num="${index}">
+                    <span>${item.content}</span>
+                </label>
+                <a href="#" class="delete" data-num="${index}">
+                    <i class="fa fa-times" data-num="${index}"></i>
+                </a>
+            </li>`;
+            
+            });
+    console.log("目前的 HTML 字串內容：", str);
+    ul.innerHTML = str;
 }
